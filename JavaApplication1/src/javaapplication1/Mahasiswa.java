438cf75823f995c1f56d464a7756091f9bdf50e5
Asus A450LC <Asus A450LC@10.5.248.45>
2016-03-17 15:07:45 +0700

    Class Mahasiswa finish

---
+++ JavaApplication1/src/javaapplication1/Mahasiswa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Asus A450LC
+ */
+public class Mahasiswa {
+    private String nim;
+    private String kelas;
+    
+    public Mahasiswa(String nama, String nim, String kelas){
+        super(nama);
+        this.nim=nim;
+        this.kelas=kelas;
+    }
+    
+    public Mahasiswa(String nama, String nim){
+        super(nama);
+        this.nim=nim;
+    }
+    
+    public String getNama(){
+        return super(nama);
+    }
+    
+    public void setNim(String nim){
+        this.nim=nim;
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void setKelas(String kelas){
+        this.kelas=kelas;
+    }
+    public String getKelas(){
+        return kelas;
+    }
+}
