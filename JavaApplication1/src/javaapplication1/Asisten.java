dd86dfc73ff755ffb6703fea0bdc10aa77d2aba7
KARTIKAAM <KARTIKAAM@10.4.248.186>
2016-03-18 13:12:13 +0700

    done tugas besar

---
+++ JavaApplication1/src/javaapplication1/Asisten.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author KARTIKAAM
+ */
+public class Asisten extends orang {
+     private ArrayList<TugasBesar> tb = new ArrayList<>();
+
+    public Asisten(String Nama) {
+        super(Nama);
+    }
+    
+    public void createTugasBesar(String judul){
+        TugasBesar tb = new TugasBesar();
+        tb.setJudul(judul);
+        this.tb.add(tb);
+    }
+    
+    public void removeTugasBesar(int indeks){
+        tb.remove(indeks);
+    }
+     
+     
+}
